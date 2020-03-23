3e21c36bb18848c320fa954ba8d64ccfdfdce184
Nathalie <n.awitor@gmx.de>
2020-03-23 15:34:01 +0100

    Adjust editor config

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,10 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'object-shorthand': 'off',
     'func-names': 'off',
     'max-len': 'off',
-    'no-console': 'off',
-    'linebreak-style': 'windows',
   },
 };
